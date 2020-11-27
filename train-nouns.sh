8656ebe31910b5c5c22b5570ce4ea68736811a22
davide <davide@pop-os.localdomain>
2020-11-27 18:53:48 +0000

    reset training params

--- train-nouns.sh
+++ train-nouns.sh
@@ -45,7 +45,7 @@ python3 embed.py \
   -neg_multiplier 0.1 \
   -lr_type constant \
   -gpu 0 \
-  -train_threads 2 \
+  -train_threads 10 \
   -dampening 1.0 \
   -batchsize 50 \
   -manifold "$MODEL" \
