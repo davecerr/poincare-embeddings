ccd441975490974e479d5769bac3cfd99ee48d68
davide <davide@pop-os.localdomain>
2020-11-28 16:56:54 +0000

    testing control functionality

--- embed.py
+++ embed.py
@@ -32,7 +32,7 @@ def reconstruction_eval(adj, opt, epoch, elapsed, loss, pth, best):
     model.load_state_dict(chkpnt['model'])
 
     meanrank, maprank = eval_reconstruction(adj, model)
-    print(f"model shape = {model.lt.shape}")
+    print(f"model shape = {model.lt}")
     sqnorms = model.manifold.norm(model.lt)
     return {
         'epoch': epoch,
