c736a3d9874988b5ad5e9df93862bf9574669ef9
davide <davide@pop-os.localdomain>
2020-12-07 20:59:58 +0000

    wip changes

--- embed.py
+++ embed.py
@@ -178,7 +178,11 @@ def main():
         log.warning(f'Specified hogwild training with GPU, defaulting to CPU...')
 
     # set default tensor type
-    th.set_default_tensor_type('torch.cuda.DoubleTensor')
+    if opt.gpu == -1:
+        th.set_default_tensor_type('torch.DoubleTensor')
+    if opt.gpu == 1:
+        th.set_default_tensor_type('torch.cuda.DoubleTensor')
+        
     # set device
     device = th.device(f'cuda:{opt.gpu}' if opt.gpu >= 0 else 'cpu')
     print(f"\n\n opt.gpu = {opt.gpu} \n DEVICE = {device} \n\n")
